da747ae026a067883a6973d9804f1547293f13ef
ankitarora <ankit992@live.in>
2020-04-26 23:53:54 +0530

    Adding datasource abstract classes and repository base impl

--- clean_architecture_number_trivia/lib/core/error/failures.dart
+++ clean_architecture_number_trivia/lib/core/error/failures.dart
@@ -5,3 +5,8 @@ import 'package:equatable/equatable.dart';
 abstract class Failure extends Equatable {
   Failure([List properties = const <dynamic>[]]) : super(properties);
 }
+
+// General failures
+class ServerFailure extends Failure {}
+
+class CacheFailure extends Failure {}
